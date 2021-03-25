fd6f7f5fc959dc45374c6cf8a15b93ef941b1f55
Nathan Edwards <edwardsnj@gmail.com>
2021-03-25 12:35:44 -0400

    more tweaks to release script

--- make_release.sh
+++ make_release.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+set -x
 VER=`python3 cksum.py --version | tr -d -c '0-9.'`
-DCCVER="CPTAC-DCC Tools v$VER"
+DCCVER="CPTAC-DCC-Tools-v$VER"
 PORVER="CPTAC Portal Tools v$VER"
-ulbin.sh
-ulbin_public.sh
-ulsrc.sh
-ulsrc_public.sh
-gh release create "$DCCVER" dist/cptacdcc-$VER.*.tgz
-gh release create "$PORVER" dist/cptacpublic-$VER.*.tgz
+./ulbin.sh
+./ulbin_public.sh
+./ulsrc.sh
+./ulsrc_public.sh
+md5sum dist/cptacdcc-$VER.*.tgz > dist/cptacdcc-$VER.md5
+md5sum dist/cptacpublic-$VER.*.tgz > dist/cptacpublic-$VER.md5
+gh release create -F dist/cptacdcc-$VER.txt "$DCCVER" dist/cptacdcc-$VER.*.tgz dist/cptacdcc-$VER.md5
+gh release create -F dist/cptacpublic-$VER.txt "$DCCVER" dist/cptacpublic-$VER.*.tgz dist/cptacpublic-$VER.md5
