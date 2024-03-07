5962d39ba91d9e7a742e5b38af4a07ff80964064
Nathan Edwards <edwardsnj@gmail.com>
2024-03-07 13:20:48 -0500

    Update ulexe.bat

--- ulexe.bat
+++ ulexe.bat
@@ -1,4 +1,4 @@
 
-toexe
-scp dist\* nedwards@edwardslab.bmcb.georgetown.edu:projects/cptac3-cdap/cptac-dcc
-/dist
+call toexe.bat
+
+scp dist\* nedwards@edwardslab.bmcb.georgetown.edu:projects/cptac3-cdap/cptac-dcc/dist
