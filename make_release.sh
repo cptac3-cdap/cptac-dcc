71daeb2c302c912ba3e493fbbefd69eb89db2b30
Nathan Edwards <edwardsnj@gmail.com>
2025-02-03 15:35:10 -0500

    update release script

--- make_release.sh
+++ make_release.sh
@@ -5,15 +5,15 @@ DCCVER="CPTAC-DCC-Tools-v$VER"
 ./ulbin.sh
 ./ulsrc.sh
 rm -f dist/cptacdcc-$VER.txt
-( cd dist; md5sum cptacdcc-$VER.*.{tgz,zip,exe} > cptacdcc-$VER.md5 ; touch cptacdcc-$VER.txt )
+( cd dist; md5sum cptacdcc-$VER.*.tgz > cptacdcc-$VER.md5 ; touch cptacdcc-$VER.txt )
 if [ "$1" ]; then 
   for comment in "$@"; do 
     echo "* $comment" >> dist/cptacdcc-$VER.txt
     # echo "* $comment" >> dist/cptacpublic-$VER.txt
   done
 fi
-gh release create "$DCCVER" dist/cptacdcc-$VER.*.{tgz,zip,exe} dist/cptacdcc-$VER.md5 -F dist/cptacdcc-$VER.txt -t "$DCCVER"
-for a in dist/cptacdcc-$VER.*.{tgz,zip,exe}; do
+gh release create "$DCCVER" dist/cptacdcc-$VER.*.tgz dist/cptacdcc-$VER.md5 -F dist/cptacdcc-$VER.txt -t "$DCCVER"
+for a in dist/cptacdcc-$VER.*.tgz; do
   a1=`basename $a`
   rclone copyto $a cptac-s3:cptac-cdap.georgetown.edu/$a1
   b1=`echo $a1 | sed "s/-$VER//"`
